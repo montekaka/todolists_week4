3dbcc720f7748fa1423dbd2ad9624fc1b5e11f73
montekaka <josh.chen.jia@gmail.com>
2015-11-04 22:57:34 -0800

    add validatoin to User and Profile models

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,23 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validate :first_name_or_last_name_both_null, :gender_either_male_or_female, :male_can_not_have_first_name_sue
+
+  def first_name_or_last_name_both_null
+  	if first_name.nil? && last_name.nil?
+  		errors.add(:first_name, "and last name can not both be null")
+  	end
+  end
+
+  def gender_either_male_or_female
+  	if ['male','female'].include?(gender) == false
+  		errors.add(:gender, "to be either male or female")
+  	end
+  end
+
+  def male_can_not_have_first_name_sue
+  	if gender == 'male' && first_name == "Sue"
+  		errors.add(:gender, "can not have the first name Sue")
+  	end
+  end
 end
