d27ca5f503e2502b38a0f61d25e856b4d0efd6d3
montekaka <josh.chen.jia@gmail.com>
2015-11-16 23:16:35 -0800

    Update TodoList and TodoItem views to adjust the links

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,62 +1,65 @@
 class TodoItemsController < ApplicationController
-  before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_todo_list, only: [:show, :edit, :update, :destroy, :new, :create]
+  before_action :set_todo_item, only: [:show, :edit, :update, :destroy]  
 
-  # GET /todo_items
-  # GET /todo_items.json
-  def index
-    @todo_items = TodoItem.all
-  end
+  # GET /todo_list/:todo_list_id/todo_items
+  # GET /todo_list/:todo_list_id/todo_items.json
+  # def index
+  #   @todo_items = TodoItem.all
+  # end
 
-  # GET /todo_items/1
-  # GET /todo_items/1.json
+  # GET /todo_list/:todo_list_id/todo_items/1
+  # GET /todo_list/:todo_list_id/todo_items/1.json
   def show
   end
 
-  # GET /todo_items/new
-  def new
-    @todo_item = TodoItem.new
+  # GET /todo_list/:todo_list_id/todo_items/new
+  def new    
+    #@todo_item = TodoItem.new
+    @todo_item = @todo_list.todo_items.new
   end
 
-  # GET /todo_items/1/edit
+  # GET /todo_list/:todo_list_id/todo_items/1/edit
   def edit
+    @show_completed_in_form = true
   end
 
-  # POST /todo_items
-  # POST /todo_items.json
+  # POST /todo_list/:todo_list_id/todo_items
+  # POST /todo_list/:todo_list_id/todo_items.json
   def create
-    @todo_item = TodoItem.new(todo_item_params)
+    @todo_item = @todo_list.todo_items.new(todo_item_params)
 
     respond_to do |format|
       if @todo_item.save
-        format.html { redirect_to @todo_item, notice: 'Todo item was successfully created.' }
-        format.json { render :show, status: :created, location: @todo_item }
+        format.html { redirect_to @todo_list, notice: 'Todo item was successfully created.' }
+        format.json { render :show, status: :created, location: @todo_list }
       else
         format.html { render :new }
-        format.json { render json: @todo_item.errors, status: :unprocessable_entity }
+        format.json { render json: @todo_list.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /todo_items/1
-  # PATCH/PUT /todo_items/1.json
+  # PATCH/PUT /todo_list/:todo_list_id/todo_items/1
+  # PATCH/PUT /todo_list/:todo_list_id/todo_items/1.json
   def update
     respond_to do |format|
       if @todo_item.update(todo_item_params)
-        format.html { redirect_to @todo_item, notice: 'Todo item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @todo_item }
+        format.html { redirect_to @todo_list, notice: 'Todo item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @todo_list }
       else
         format.html { render :edit }
-        format.json { render json: @todo_item.errors, status: :unprocessable_entity }
+        format.json { render json: @todo_list.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /todo_items/1
-  # DELETE /todo_items/1.json
+  # DELETE /todo_list/:todo_list_id/todo_items/1
+  # DELETE /todo_list/:todo_list_id/todo_items/1.json
   def destroy
     @todo_item.destroy
     respond_to do |format|
-      format.html { redirect_to todo_items_url, notice: 'Todo item was successfully destroyed.' }
+      format.html { redirect_to @todo_list, notice: 'Todo item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -64,7 +67,12 @@ class TodoItemsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_todo_item
-      @todo_item = TodoItem.find(params[:id])
+      #@todo_item = TodoItem.find(params[:id])
+      @todo_item = TodoList.find(params[:todo_list_id]).todo_items.find(params[:id])
+    end
+
+    def set_todo_list
+      @todo_list = TodoList.find(params[:todo_list_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
     
   resources :todo_lists do 
     resources :todo_items
-    
   end
   root 'todo_lists#index'
 
