aca1d1ad53dc3865783fac4e023e98644beb8372
montekaka <josh.chen.jia@gmail.com>
2015-11-17 22:17:28 -0800

    Implemented the Sessions controller class and view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	username = params[:user][:username]
+  	password = params[:user][:password]
+  	user = User.find_by(username: username)  	
+
+  	if user && user.authenticate(password)
+  		session[:user_id] = user.id
+  		redirect_to root_path, notice: "Logged in successfully"
+  	else
+  		redirect_to login_path, alert: "Invalid username/password combination"
+  	end
   end
 
   def destroy
+  	reset_session
+  	redirect_to login_path, notice: "You have been logged out"
   end
 end
